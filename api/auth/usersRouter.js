7723e26eab2c463d66999545631a8d722eee60f9
tamara-f <filatovatam@ukr.net>
2020-12-09 12:00:40 +0200

    added hw-05

--- api/auth/usersRouter.js
+++ api/auth/usersRouter.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const upload = require('../helpers/multer.js');
+const moveFileToPublic = require('../helpers/moveFile');
 
 const AuthController = require('./authController');
 
@@ -8,6 +10,14 @@ UserRouter.get(
   '/current',
   AuthController.authorize,
   AuthController.getUserController
+);
+
+UserRouter.patch(
+  '/avatar',
+  upload.single('avatar'),
+  AuthController.authorize,
+  moveFileToPublic,
+  AuthController.updateUser
 );
 
 module.exports = UserRouter;
